aa5999ed416b871b0479be4ec96b4168bfc4cb0e
alirafmc <alirafmc@gmail.com>
2019-08-26 13:28:33 +0700

    beranda

--- application/views/user/beranda.php
+++ application/views/user/beranda.php
@@ -20,33 +20,33 @@
 
 !-- Start cat Area -->
 <section class="cat-area section-gap" id="feature">
+	<div class="col-md-12 pb-30 header-text text-center">
+		<h1 class="mb-10">Layanan kami</h1>
+		<p>
+			PT. Patra Darma Wijaya Bandung
+		</p>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-4">
 				<div class="single-cat d-flex flex-column">
 					<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
 					<h4 class="mb-20" style="margin-top: 23px;">Firma Hukum</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
-					</p>
+
 				</div>
 			</div>
 			<div class="col-lg-4">
 				<div class="single-cat">
 					<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
 					<h4 class="mt-40 mb-20">Perdagangan Minyak dan Gas</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
-					</p>
+
 				</div>
 			</div>
 			<div class="col-lg-4">
 				<div class="single-cat">
 					<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></a>
 					<h4 class="mt-40 mb-20">Logistik</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why.
-					</p>
+
 				</div>
 			</div>
 		</div>
@@ -55,162 +55,14 @@
 <!-- End cat Area -->
 
 
-<!-- Start home-about Area -->
-<section class="home-about-area section-gap" id="about">
-	<div class="container">
-		<div class="row justify-content-center align-items-center">
-			<div class="col-lg-8 col-md-12 home-about-left">
-				<h6>Brand new app to blow your mind</h6>
-				<h1>
-					We’ve made a life <br>
-					that will change you
-				</h1>
-				<p class="sub">We are here to listen from you deliver exellence</p>
-				<p class="pb-20">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
-				</p>
-				<a class="primary-btn" href="#">Get Started Now</a>
-			</div>
-			<div class="col-lg-4 col-md-12 home-about-right relative">
-				<form class="form-wrap" action="#">
-					<h4 class="text-white pb-20">Request a Quote</h4>
-					<div class="form-select" id="service-select"">
-									<select>
-										<option value=" 1">Select Service</option>
-						<option value="2">Service One</option>
-						<option value="3">Service Two</option>
-						<option value="4">Service Three</option>
-						<option value="5">Service Four</option>
-						</select>
-					</div>
-					<input type="text" class="form-control" placeholder="name">
-					<input type="phone" class="form-control" placeholder="Phone Number">
-					<input type="email" class="form-control" placeholder="Email Address">
-					<textarea name="message" id="" cols="30" rows="5" placeholder="Message" class="form-control"></textarea>
-					<button class="primary-btn">Request Free Quote</button>
-				</form>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- End home-about Area -->
-
-
-<!-- Start service Area -->
-<section class="service-area section-gap" id="service">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-12 pb-30 header-text text-center">
-				<h1 class="mb-10">Our Capturing Market Sectors</h1>
-				<p>
-					Who are in extremely love with eco friendly system..
-				</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-4">
-				<div class="single-service">
-					<div class="thumb">
-						<img src="img/s1.jpg" alt="">
-					</div>
-					<h4>Automotive Engineering</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
-					</p>
-				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="single-service">
-					<div class="thumb">
-						<img src="img/s2.jpg" alt="">
-					</div>
-					<h4>Construction & Engineering</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
-					</p>
-				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="single-service">
-					<div class="thumb">
-						<img src="img/s3.jpg" alt="">
-					</div>
-					<h4>Industrial Engineering</h4>
-					<p>
-						inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct women face higher conduct.
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- End service Area -->
-
-
-<!-- Start faq Area -->
-<section class="faq-area section-gap relative">
-	<div class="overlay overlay-bg"></div>
-	<div class="container">
-		<div class="row justify-content-center align-items-center">
-			<div class="col-lg-3 col-md-6">
-				<div class="single-faq">
-					<div class="circle">
-						<div class="inner"></div>
-					</div>
-					<h5><span class="counter">2</span>K+</h5>
-					<p>
-						Projects Completed
-					</p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="single-faq">
-					<div class="circle">
-						<div class="inner"></div>
-					</div>
-					<h5><span class="counter">5.5</span>K</h5>
-					<p>
-						Total Employees
-					</p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="single-faq">
-					<div class="circle">
-						<div class="inner"></div>
-					</div>
-					<h5 class="counter">959</h5>
-					<p>
-						Happy Clients
-					</p>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-6">
-				<div class="single-faq">
-					<div class="circle">
-						<div class="inner"></div>
-					</div>
-					<h5 class="counter">367</h5>
-					<p>
-						Tickets Submited
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- End faq Area -->
-
-
-
 <!-- Start blog Area -->
 <section class="blog-area section-gap" id="blog">
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-12 pb-30 header-text">
-				<h1>Latest posts from our Blog</h1>
+				<h1>Produk Kami</h1>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> labore et dolore magna aliqua.
+					PT. Patra Darma Wijaya Bandung
 				</p>
 			</div>
 		</div>
@@ -219,63 +71,33 @@
 				<div class="thumb">
 					<img class="f-img img-fluid mx-auto" src="img/b1.jpg" alt="">
 				</div>
-				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
-					<div>
-						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
-					</div>
-					<div class="meta">
-						13th Dec
-						<span class="lnr lnr-heart"></span> 15
-						<span class="lnr lnr-bubble"></span> 04
-					</div>
-				</div>
+
 				<a href="#">
-					<h4>Portable Fashion for young women</h4>
+					<h4>JA SADEWA & REKAN</h4>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea.
+					<strong>JA Sadewa & Rekan </strong> melayani berbagai jenis permasalahan hukum, mulai dari perkawinan, hutang piutang, waris, bisnis, kontrak usaha, ketenagakerjaan dan lainnya. Kami membantu Anda mendapatkan panduan dan pencerahan hukum dari mitra advokat yang kredibel, dengan biaya yang terukur dan transparan.
 				</p>
 			</div>
 			<div class="single-blog col-lg-4 col-md-4">
 				<div class="thumb">
 					<img class="f-img img-fluid mx-auto" src="img/b2.jpg" alt="">
 				</div>
-				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
-					<div>
-						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
-					</div>
-					<div class="meta">
-						13th Dec
-						<span class="lnr lnr-heart"></span> 15
-						<span class="lnr lnr-bubble"></span> 04
-					</div>
-				</div>
+
 				<a href="#">
-					<h4>Portable Fashion for young women</h4>
+					<h4>PATRA DARMA WIJAYA OIL & GAS</h4>
 				</a>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea.
+					<strong>Patra Darma Oil & Gas </strong> melayani penjualan dan konsultasi Minyak dan Gas. Kami membantu Anda mendapatkan panduan dan informasi perihal minyak dan gas secara online dan transparan.
 				</p>
 			</div>
 			<div class="single-blog col-lg-4 col-md-4">
 				<div class="thumb">
 					<img class="f-img img-fluid mx-auto" src="img/b3.jpg" alt="">
 				</div>
-				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
-					<div>
-						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
-					</div>
-					<div class="meta">
-						13th Dec
-						<span class="lnr lnr-heart"></span> 15
-						<span class="lnr lnr-bubble"></span> 04
-					</div>
-				</div>
+
 				<a href="#">
-					<h4>Portable Fashion for young women</h4>
+					<h4>FERDINAL LOGISTIK</h4>
 				</a>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea.
@@ -294,23 +116,17 @@
 		<div class="row">
 			<div class="col-lg-5 col-md-6 col-sm-6">
 				<div class="single-footer-widget">
-					<h6>About Us</h6>
+					<h6>Tentang Kami</h6>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+					Patra Darma Wijaya Bandung melayani konsultasi secara online
 					</p>
-					<p class="footer-text">
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>
-							document.write(new Date().getFullYear());
-						</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</p>
+					
 				</div>
 			</div>
 			<div class="col-lg-5  col-md-6 col-sm-6">
 				<div class="single-footer-widget">
-					<h6>Newsletter</h6>
-					<p>Stay update with our latest</p>
+					<h6>Berita Acara</h6>
+					
 					<div class="" id="mc_embed_signup">
 						<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
 							<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
@@ -326,8 +142,8 @@
 			</div>
 			<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
 				<div class="single-footer-widget">
-					<h6>Follow Us</h6>
-					<p>Let us be social</p>
+					<h6>Sosial Media</h6>
+					
 					<div class="footer-social d-flex align-items-center">
 						<a href="#"><i class="fa fa-facebook"></i></a>
 						<a href="#"><i class="fa fa-twitter"></i></a>
--- application/views/user/info_minyak.php
+++ application/views/user/info_minyak.php
@@ -185,23 +185,17 @@
 		<div class="row">
 			<div class="col-lg-5 col-md-6 col-sm-6">
 				<div class="single-footer-widget">
-					<h6>About Us</h6>
+					<h6>Tentang Kami</h6>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+					Patra Darma Wijaya Bandung melayani konsultasi secara online
 					</p>
-					<p class="footer-text">
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>
-							document.write(new Date().getFullYear());
-						</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</p>
+					
 				</div>
 			</div>
 			<div class="col-lg-5  col-md-6 col-sm-6">
 				<div class="single-footer-widget">
-					<h6>Newsletter</h6>
-					<p>Stay update with our latest</p>
+					<h6>Berita Acara</h6>
+					
 					<div class="" id="mc_embed_signup">
 						<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
 							<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
@@ -217,8 +211,8 @@
 			</div>
 			<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
 				<div class="single-footer-widget">
-					<h6>Follow Us</h6>
-					<p>Let us be social</p>
+					<h6>Sosial Media</h6>
+					
 					<div class="footer-social d-flex align-items-center">
 						<a href="#"><i class="fa fa-facebook"></i></a>
 						<a href="#"><i class="fa fa-twitter"></i></a>
