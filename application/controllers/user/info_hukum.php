7d489f7c753bfac515356b85e0ef1ff0925586c6
alirafmc <alirafmc@gmail.com>
2019-08-26 09:54:41 +0700

    Penambhan FrontRnd

--- application/controllers/user/info_hukum.php
+++ application/controllers/user/info_hukum.php
@@ -9,4 +9,37 @@ class Info_hukum extends CI_Controller
         $this->load->view('user/info_hukum');
         $this->load->view('templates_user/footer');
     }
+
+    public function kirim_pesan()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->index();
+        } else {
+            $id = $this->input->post('id');
+            $nama = $this->input->post('nama');
+            $email = $this->input->post('email');
+            $pesan = $this->input->post('pesan');
+
+            $data = array(
+                'nama' => $nama,
+                'email' => $email,
+                'pesan' => $pesan,
+            );
+
+            $this->hubungi_model->insert_data($data, 'hubungi');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">
+            Pesan Berhasil Dikirim!!
+          </div>');
+            redirect('user/info_hukum');
+        }
+    }
+
+    public function _rules()
+    {
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('pesan', 'Pesan', 'required');
+    }
 }
