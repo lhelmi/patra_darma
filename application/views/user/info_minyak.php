dcc50f3cbf9bae1269fdfe86175f043c813ac287
alirafmc <alirafmc@gmail.com>
2019-09-03 21:58:02 +0700

    Perbaikan FrontEnd Isi

--- application/views/user/info_minyak.php
+++ application/views/user/info_minyak.php
@@ -22,40 +22,7 @@
 
 <!-- Start home-about Area -->
 <section class="home-about-area section-gap" id="about">
-	<div class="container">
-		<div class="row justify-content-center align-items-center">
-			<div class="col-lg-8 col-md-12 home-about-left">
-				<h6>Brand new app to blow your mind</h6>
-				<h1>
-					We’ve made a life <br>
-					that will change you
-				</h1>
-				<p class="sub">We are here to listen from you deliver exellence</p>
-				<p class="pb-20">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
-				</p>
-				<a class="primary-btn" href="#">Get Started Now</a>
-			</div>
-			<div class="col-lg-4 col-md-12 home-about-right relative">
-				<form class="form-wrap" action="#">
-					<h4 class="text-white pb-20">Request a Quote</h4>
-					<div class="form-select" id="service-select"">
-									<select>
-										<option value=" 1">Select Service</option>
-						<option value="2">Service One</option>
-						<option value="3">Service Two</option>
-						<option value="4">Service Three</option>
-						<option value="5">Service Four</option>
-						</select>
-					</div>
-					<input type="text" class="form-control" placeholder="name">
-					<input type="phone" class="form-control" placeholder="Phone Number">
-					<input type="email" class="form-control" placeholder="Email Address">
-					<textarea name="message" id="" cols="30" rows="5" placeholder="Message" class="form-control"></textarea>
-					<button class="primary-btn">Request Free Quote</button>
-				</form>
-			</div>
-		</div>
+
 	</div>
 </section>
 <!-- End home-about Area -->
@@ -79,10 +46,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -101,10 +68,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -124,10 +91,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -147,10 +114,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -170,10 +137,9 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -193,10 +159,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -216,10 +182,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -239,10 +205,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
@@ -262,10 +228,10 @@
 				<div class="bottom d-flex justify-content-between align-items-center flex-wrap">
 					<div>
 						<img class="img-fluid" src="img/user.png" alt="">
-						<a href="#"><span>Mark Wiens</span></a>
+						<a href="#"><span></span></a>
 					</div>
 					<div class="meta">
-						13th Dec
+
 						<!-- <span class="lnr lnr-heart"></span> 15
 						<span class="lnr lnr-bubble"></span> 04 -->
 					</div>
--- application/views/user/konsultasi.php
+++ application/views/user/konsultasi.php
@@ -7,7 +7,7 @@
                 <h1 class="text-white">
                     Konsultasi
                 </h1>
-                <p class="text-white link-nav"><a href="">Home </a> <span class="lnr lnr-arrow-right"></span> <a href=""> Contact Us</a></p>
+
             </div>
         </div>
     </div>
@@ -18,7 +18,7 @@
 <section class="contact-page-area section-gap">
     <div class="container">
         <div class="row">
-           
+
             <div class="col-lg-4 d-flex flex-column address-wrap">
                 <div class="single-contact-address d-flex flex-row">
                     <div class="icon">
@@ -26,7 +26,7 @@
                     </div>
                     <div class="contact-details">
                         <h5>Margaasih, Bandung</h5>
-                        <p>56/8, West Panthapath</p>
+                        <p></p>
                     </div>
                 </div>
                 <div class="single-contact-address d-flex flex-row">
@@ -34,8 +34,8 @@
                         <span class="lnr lnr-phone-handset"></span>
                     </div>
                     <div class="contact-details">
-                        <h5>00 (880) 9865 562</h5>
-                        <p>Mon to Fri 9am to 6 pm</p>
+                        <h5>0812-1469-9108</h5>
+                        <p></p>
                     </div>
                 </div>
                 <div class="single-contact-address d-flex flex-row">
@@ -43,8 +43,8 @@
                         <span class="lnr lnr-envelope"></span>
                     </div>
                     <div class="contact-details">
-                        <h5>patradarmawijaya@gmail.com</h5>
-                        <p>Send us your query anytime!</p>
+                        <h5>patradarmawja@gmail.com</h5>
+                        <p>Konsultasikan bersama kami</p>
                     </div>
                 </div>
             </div>
@@ -53,16 +53,16 @@
                     <div class="row">
                         <?php echo $this->session->flashdata('pesan') ?>
                         <div class="col-lg-6 form-group">
-                            <input type="text" name="nama" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mb-20 form-control" required="" type="text">
+                            <input type="text" name="nama" placeholder="Nama" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mb-20 form-control" required="" type="text">
 
-                            <input type="text" name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control" required="" type="email">
+                            <input type="text" name="email" placeholder="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control" required="" type="email">
 
-                            <input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
+                            <input name="subject" placeholder="Pesan" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
                             <div class="mt-20 alert-msg" style="text-align: left;"></div>
                         </div>
                         <div class="col-lg-6 form-group">
                             <textarea type="text" name="pesan" class="form-control" rows="6"></textarea>
-                            <button type="submit" class="genric-btn primary circle mt-30" style="float: right;">Send Message</button>
+                            <button type="submit" class="genric-btn primary circle mt-30" style="float: right;">Kirim Pesan</button>
                         </div>
                     </div>
                 </form>
@@ -71,4 +71,3 @@
     </div>
 </section>
 <!-- End contact-page Area -->
-
